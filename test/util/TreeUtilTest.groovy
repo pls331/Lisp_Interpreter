423dba02c68d39d8a7940a2e5c2f419eb030ca4b
Lowson_PAN <panluoshang@live.com>
2017-04-20 21:32:17 -0400

    fixed bug of list notation

--- test/util/TreeUtilTest.groovy
+++ test/util/TreeUtilTest.groovy
@@ -5,18 +5,21 @@ package util
 class TreeUtilTest extends GroovyTestCase {
 
     void testPrettyPrint() {
-
-        TreeNode node0 = new TreeNode(new Token("1", TokenType.NUMERIC_ATOM))
+        TreeNode nodeR1 = new TreeNode(new Token("0", TokenType.NUMERIC_ATOM))
         TreeNode node1 = new TreeNode(new Token("1", TokenType.NUMERIC_ATOM))
-        TreeNode node1L = new TreeNode()
+        TreeNode nodeR2 = new TreeNode()
         TreeNode node2 = new TreeNode(new Token("2", TokenType.NUMERIC_ATOM))
+        TreeNode nodeR3 = new TreeNode()
         TreeNode node3 = new TreeNode(new Token("3", TokenType.NUMERIC_ATOM))
         TreeNode node4 = TreeUtil.nodeNIL
-        println TreeUtil.getListNotation(node1)
-        node.setLeft(node2)
-        node.setRight(node4)
-        node2.setLeft(node3)
-        println TreeUtil.getListNotation(node)
+        println TreeUtil.getListNotation(nodeR1)
+        nodeR1.setLeft(node1)
+        nodeR1.setRight(nodeR2)
+        nodeR2.setLeft(node2)
+        nodeR2.setRight(nodeR3)
+        nodeR3.setLeft(node3)
+        nodeR3.setRight(node3)
+        println TreeUtil.getListNotation(nodeR1)
     }
 
 
